03fbf78cbe51c7d89fba3e3364f85d3a60c53312
Alex Narvaez-Duckworth <anduckworth00@gmail.com>
2022-11-06 02:19:00 -0800

    local

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from dune import execute_query
-from dune import execute_query_with_params
 from dune import get_query_results
 from dune import get_query_status
 import time
@@ -8,24 +7,33 @@ import time
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
-def get_query():
-    execution_id = execute_query(1530879)#, params = {"Period" : ["1 hour", "4 hour", "12 hour", "24 hour"], "USD amount per transaction" : [500, 1000, 5000, 10000]})
-    #print(execution_id)
-    #print(time.time())
+@app.route('/get_db/<string:pageID>', methods=["GET"])
+def get_query(pageID:str):
+    execution_id = execute_query(1530879)
     while True:
         response = get_query_status(execution_id)
     
-        #print(response)
+       
         response_status = response.json()['state']
         
         if response_status == 'QUERY_STATE_COMPLETED':
             data = get_query_results(execution_id).json()
-            #print(time.time())
+            
             return data
         elif response_status == 'QUERY_STATE_EXECUTING':
             time.sleep(.25)
         else: 
             return response_status
+
+@app.route('/get_db', methods =["POST", "GET"])
+def web_page_interactions():
+    if request.method == "POST":
+        ID = request.form["pageID"]
+        redirect(url_for("pageID", pageID=ID))
+    else: 
+        return render_template('template1.html')
+
 if __name__ == '__main__':
     app.run()
+
+
